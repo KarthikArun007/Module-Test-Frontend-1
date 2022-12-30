63c1370f61adfbb06409df1c8039b1f220cf8fae
KarthikArun007 <63771132+KarthikArun007@users.noreply.github.com>
2022-12-30 20:30:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showm() {
+    var A1 = document.getElementById('A1');
+    var A2 = document.getElementById('A2');
+    var A3 = document.getElementById('A3');
+
+    if (A1.value === '' | A2.value === '' | A3.value === '') {
+
+    } else {
+        document.getElementById('message').style.opacity = '1';
+    }
+}
